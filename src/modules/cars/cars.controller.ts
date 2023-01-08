3d53c624eccd83e8e37ffbb8f87b2999ec189a90
Quy Nguyen Duong <quynd1@rising-stars.vn>
2023-01-08 07:39:52 +0700

    Format data response cars API

--- src/modules/cars/cars.controller.ts
+++ src/modules/cars/cars.controller.ts
@@ -3,7 +3,7 @@ import { CarsService } from './cars.service';
 import { TransformPlainToInstance } from 'class-transformer';
 import { CarsDto } from './dto/cars.dto';
 import { CarsRequestParamsDto } from './dto/cars-request-params.dto';
-import { ApiQuery, ApiTags } from '@nestjs/swagger';
+import { ApiHeader, ApiQuery, ApiTags } from '@nestjs/swagger';
 
 @Controller('cars')
 @ApiTags('api/v1/cars')
@@ -12,11 +12,30 @@ export class CarsController {
 
   @Get()
   @ApiQuery({ type: CarsRequestParamsDto })
-  @TransformPlainToInstance(CarsDto)
-  async findAll(@Query() query, @Headers('accept-language') lang) {
+  @ApiHeader({
+    name: 'accept-language',
+  })
+  async findAll(@Query() query, @Headers('accept-language') lang: string) {
     const { data, panigation } = await this.carsService.findAll(query, lang);
 
-    return { items: data, panigation };
+    return {
+      items: data.map((item) => {
+        return {
+          id: item.id,
+          capacity: item.capacity,
+          gasoline: item.gasoline,
+          base_price: item.base_price,
+          price: item.price,
+          name: item.car_translation?.name,
+          steering: item.car_translation?.steering,
+          car_types: item.car_types.map((type) => ({
+            id: type.master_type?.master_type_translation?.id,
+            name: type.master_type?.master_type_translation?.name,
+          })),
+        };
+      }),
+      panigation,
+    };
   }
 
   @Get(':id')
