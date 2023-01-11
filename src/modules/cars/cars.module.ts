254c1f0c35c14f12be26173e984f878f9a210cdc
Quy Nguyen Duong <quynd1@rising-stars.vn>
2023-01-11 16:48:34 +0700

    Create cars module

---
+++ src/modules/cars/cars.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { CarsService } from './cars.service';
+import { CarsController } from './cars.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Car } from './entities/car.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Car])],
+  controllers: [CarsController],
+  providers: [CarsService],
+})
+export class CarsModule {}
---
+++ src/modules/cars/entities/car.entity.ts
@@ -0,0 +1,61 @@
+import { DateTimeEntity } from 'src/common/base-entity/date-time.entity';
+import { CarImage } from 'src/modules/car-images/entities/car-image.entity';
+import { CarLocation } from 'src/modules/car-locations/entities/car-location.entity';
+import { CarTranslation } from 'src/modules/car-translations/entities/car-translation.entity';
+import { CarType } from 'src/modules/car-types/entities/car-type.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({
+  name: 'cars',
+})
+export class Car extends DateTimeEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    nullable: true,
+    type: 'int',
+  })
+  capacity: number;
+
+  @Column({
+    nullable: true,
+    type: 'int',
+  })
+  gasoline: number;
+
+  @Column({
+    nullable: true,
+    type: 'int',
+  })
+  quantity: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2, nullable: true })
+  base_price: string;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2, nullable: true })
+  price: number;
+
+  @Column({ type: 'nvarchar', nullable: true })
+  image_thumbnail: string;
+
+  @OneToMany(() => CarLocation, (car_location) => car_location.car, {
+    createForeignKeyConstraints: false,
+  })
+  car_locations: CarLocation[];
+
+  @OneToMany(() => CarTranslation, (car_translation) => car_translation.car, {
+    createForeignKeyConstraints: false,
+  })
+  car_translations: CarTranslation[];
+
+  @OneToMany(() => CarType, (car_type) => car_type.car, {
+    createForeignKeyConstraints: false,
+  })
+  car_types: CarType[];
+
+  @OneToMany(() => CarImage, (car_image) => car_image.car, {
+    createForeignKeyConstraints: false,
+  })
+  car_images: CarImage[];
+}
