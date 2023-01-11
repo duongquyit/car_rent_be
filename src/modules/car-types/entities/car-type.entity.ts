ce7ee9db8b5c69a8275e2f10b1ff198a71d021f1
Quy Nguyen Duong <quynd1@rising-stars.vn>
2023-01-11 16:53:36 +0700

    implement create car types

---
+++ src/modules/car-types/entities/car-type.entity.ts
@@ -0,0 +1,43 @@
+import { DateTimeEntity } from 'src/common/base-entity/date-time.entity';
+import { Car } from 'src/modules/cars/entities/car.entity';
+import { MasterType } from 'src/modules/master-types/entities/master_type.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  JoinTable,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({
+  name: 'car_types',
+})
+export class CarType extends DateTimeEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    nullable: true,
+    type: 'int',
+  })
+  car_id: number;
+
+  @Column({
+    nullable: true,
+    type: 'int',
+  })
+  type_id: number;
+
+  @ManyToOne(() => MasterType, (master_type) => master_type.types, {
+    createForeignKeyConstraints: false,
+  })
+  @JoinColumn({ name: 'type_id' })
+  master_type: MasterType;
+
+  @ManyToOne(() => Car, (car) => car.car_types, {
+    createForeignKeyConstraints: false,
+  })
+  @JoinColumn({ name: 'car_id' })
+  car: Car;
+}
