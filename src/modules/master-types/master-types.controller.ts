345720c75d3b76d0942d7c331842123d7d67e745
Quy Nguyen Duong <quynd1@rising-stars.vn>
2023-04-05 08:39:53 +0700

    modify get master type car

--- src/modules/master-types/master-types.controller.ts
+++ src/modules/master-types/master-types.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get, Headers } from '@nestjs/common';
 import { MasterTypesService } from './master-types.service';
 import { ApiHeader, ApiTags } from '@nestjs/swagger';
-import { I18n, I18nContext } from 'nestjs-i18n';
+import { I18n, I18nContext, I18nService } from 'nestjs-i18n';
+import { EN } from 'src/common/constants/language.constant';
 
 @Controller('master-types')
 @ApiTags('Master Types')
 export class MasterTypesController {
-  constructor(private readonly masterTypesService: MasterTypesService) {}
+  constructor(
+    private readonly masterTypesService: MasterTypesService,
+    private readonly i18nService: I18nService,
+  ) {}
 
   @Get()
   @ApiHeader({ name: 'accept-language', required: false })
@@ -14,7 +18,9 @@ export class MasterTypesController {
     @I18n() i18n: I18nContext,
     @Headers('accept-language') lang: string,
   ) {
-    const types = await this.masterTypesService.findAll(lang || i18n.lang);
+    const types = await this.masterTypesService.findAll(
+      this.i18nService.resolveLanguage(lang || EN),
+    );
 
     return {
       items: types.map((item) => item.master_type_translation),
